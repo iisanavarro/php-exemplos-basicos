76a41a6424a999336685792aa0490437efb6d3ac
iisanavarro <isamonavarro@hotmail.com>
2026-08-04 14:48:39 -0300

    Soma das variaveis

--- inicio-revisao/1_variaveis.php
+++ inicio-revisao/1_variaveis.php
@@ -1,1 +1,11 @@
-<!-- 1ª Digitação (Aqui) -->
+<?php
+
+// Criação das variáveis
+$n1 = 5;
+$n2 = 16;
+
+// Somando
+$soma = $n1 + $n2;
+
+// Exibindo o resultado
+echo "A soma de n1 + n2 é igual a: $soma";
