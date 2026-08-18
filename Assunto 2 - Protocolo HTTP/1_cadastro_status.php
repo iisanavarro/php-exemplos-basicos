8c704cb008376eefbc9012b920fc8e666ddf3083
iisanavarro <isamonavarro@hotmail.com>
2026-08-18 16:10:04 -0300

    cadastro html

---
+++ Assunto 2 - Protocolo HTTP/1_cadastro_status.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastro - Status Codes</title>
+</head>
+<body>
+
+<h1>Cadastro de Aluno (com status Code)</h1>
+<form action="" method="post">
+    <label for="nome">Nome:</label>
+    <input type="text"> name="nome" required><br><br>
+
+     <label for="Idade">Idade:</label>
+    <input type="text"> Idade="nome" required><br><br>
+
+    <button> type="submit">enviar</button>
+</form>
+    
+</body>
+</html>
